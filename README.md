184a87157c4cadd00828f0e0877ac5ff49b54fd8
reyarqueza <reyarqueza@gmail.com>
2018-05-21 18:44:02 -0700

    First version

---
+++ README.md
@@ -0,0 +1,42 @@
+# Http Cache To Mongo
+
+## Introduction
+
+The **http-cache-to-mongo** module allows you to store an http response into mongodb. This can be useful for reducing requests to an external http api / web service. 
+
+The first time you make a call to a web service, **http-cache-to-mongo** will first check if that data already exists in your mongo database based on its url.
+
+If the data for that url does not exist in your mongo database, **http-cache-to-mongo** will store that data in mongo db before returning you that data.
+
+If the data for that url exists in your mongo database, **http-cache-to-mongo** returns that data, and never makes a request to that url ever again, saving your request counts against your web service api quota.
+
+## Example
+Below is an example how you would use it. If the webservice doesn't require an apiKeyName or apiKeyValue, leave these values blank.
+
+```
+const apiKeyName = 'api_key';
+const apiKeyValue = 'asdfwyenxcvkshgdwkssnqf0vzja';
+const dbName = 'httpcachedb';
+const collectionName = 'httpcache';
+const connectionUrl = 'mongodb://localhost:27017';
+
+const cache = new HttpCacheToMongo({
+    apiKeyName,
+    apiKeyValue,
+    dbName,
+    collectionName,
+    connectionUrl
+});
+
+cache.then( (cache) => {
+    cache.get('https://api.mockupwebservice.local/abc/123')
+    .then(json => {
+        // your json data is here
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}).catch(error => {
+    console.log(error);
+});
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+const axios = require('axios');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+class HttpCacheToMongo {
+
+    constructor(obj) {
+
+        const {
+            apiKeyName, 
+            apiKeyValue, 
+            dbName,
+            collectionName,
+            connectionUrl
+        } = obj;
+
+        this.apiKeyName = apiKeyName;
+        this.apiKeyValue = apiKeyValue;
+        this.dbName = dbName;
+        this.collectionName = collectionName;
+        this.connectionUrl = connectionUrl;
+
+        return new Promise(resolve => {
+            MongoClient.connect(this.connectionUrl, (err, client) => {
+                assert.equal(null, err);       
+                this.db = client.db(this.dbName);
+                this.collection = this.db.collection(this.collectionName);
+                resolve(this);
+            });
+        });
+    }
+
+    insertDocument(json, url, callback) {
+
+        const documentByUrl = {
+            url,
+            data: json
+        }
+
+        this.collection.insert(documentByUrl, (err, result) => {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            assert.equal(1, result.ops.length);
+            callback(result.ops);
+        });
+    }
+
+    get(url) {
+
+        const absUrl = `${url}?${this.apiKeyName}=${this.apiKeyValue}`;
+        
+        return new Promise( (resolve, reject) => {
+
+            this.collection.find({
+                url
+            })
+            .toArray( (err, docs) => {
+                // if the data for this url already exists, return it.
+                if (docs.length > 0) {
+                    resolve(docs);
+                }
+                // else make a call to the webservice and store it in mongodb
+                else {
+                    axios
+                    .get(absUrl)
+                    .then(response => {
+                        this.insertDocument(response.data, url, (docs) => {
+                            const json = docs;
+                            resolve(json);
+                        });
+                    })
+                    .catch( (error) => {
+                        console.log(error);
+                        reject(error);
+                    });
+                }
+            });
+        });
+    }
+}
